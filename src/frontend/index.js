52c219088709034cd722659600c5434792eae1da
Mihai Toader <mihai@sectorlabs.ro>
2021-02-12 22:24:27 +0200

    Setup the vanillaJS frontend with webpack and all the good stuff

---
+++ src/frontend/index.js
@@ -0,0 +1,10 @@
+
+import './index.scss';
+
+const App = () => {
+  return `
+    <div class='container'> Hello World</div>
+  `
+};
+
+document.getElementById('root').innerHTML = App();
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+
+    entry: './src/frontend/index.js',
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader'
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            implementation: require('sass')
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+
+                        options: {
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'bundle.css'
+        }),
+        new HtmlWebpackPlugin({
+          template: path.join(__dirname, 'src/frontend/index.html')
+        })
+    ]
+};
