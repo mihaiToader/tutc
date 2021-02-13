da655f3e0394a6bb33e8f52c46ffe6f36d4cd11e
Mihai Toader <mihai@sectorlabs.ro>
2021-02-13 22:57:19 +0200

    Add join room section

--- src/frontend/main/homepage/joinRoom.js
+++ src/frontend/main/homepage/joinRoom.js
@@ -1,23 +1,66 @@
 import Component from 'main/component';
+import { Input, ErrorsContainer } from 'main/components';
+
 import ReturnToMenu from './returnToMenu';
 
-class JoinRoom extends Component {
+const ROOM_ERROR = 'Room not found! :(';
+
+class CreateRoom extends Component {
     constructor(props) {
-        super(props);
+        super(props, {
+            error: false,
+        });
         this.initChildComponents();
+
+        this.roomName = '';
     }
 
     initChildComponents = () => {
         this.addChild('returnToMenu', new ReturnToMenu({onClick: this.props.onBack}));
+        this.addChild('roomInput', new Input({onChange: this.onNameChange, label: 'Room', initialValue: this.roomName}));
+        this.addChild('errors', new ErrorsContainer({errors: [ROOM_ERROR], onClose: () => this.setError(false)}));
     };
 
-    isMounted = () => {
-        return this.child('returnToMenu').isMounted();
+    onMount = () => {
+        this.addEventByClassName('join', 'click', this.onJoin)
     };
+
+    onNameChange = (username) => {
+        this.username = username;
+    };
+
+    setError = (isError) => {
+        this.setState({
+            ...this.state,
+            error: isError
+        })
+    };
+
+    onJoin = () => {
+        if (!this.roomName) {
+            this.setError(true)
+        }
+
+    };
+
+    renderErrors = () => this.state.error ? `
+        <div class='create-room-errors'>
+            ${this.child('errors').render()}
+        </div>
+    ` : '';
+
 
     render = () => {
-        return `${this.child('returnToMenu').render()}`
+        return `
+            <div id='${this.id}' class="join-room-container">
+                <span class="title">Join room</span>
+                ${this.child('roomInput').render()}
+                <div class="join">Join</div>
+                ${this.renderErrors()}
+                ${this.child('returnToMenu').render()}
+            </div>
+        `
     }
 }
 
-export default JoinRoom;
+export default CreateRoom;
